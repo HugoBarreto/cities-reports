3d9e362124c3f79eec0b32a00e975b166db2ff0e
HugoBarreto <hugobarreto94@gmail.com>
2019-04-02 16:50:07 -0300

    Modified Report to show only relative data, not absolute interecations. * Fixed queries poor code (type error with values exported) * table_template.html modified
    Now sending e-mail with correct subject and body, also sending to final recipients
    (still requiring their email been validated)
    * reports_config.json modified
    
    
    Former-commit-id: 74d6cc712752c0ea5465bed9ed67a658cce90bde

--- weekly/templates/queries/ACCIDENTS.sql
+++ weekly/templates/queries/ACCIDENTS.sql
@@ -25,7 +25,7 @@ WITH s1 AS
      GROUP BY street)
 
 SELECT s1.street, share_of_total_interactions_s1, share_of_total_interactions_s2,
-       share_of_total_interactions_s1 /share_of_total_interactions_s2 - 1 AS variation
+       CAST(share_of_total_interactions_s1 AS double) / CAST(share_of_total_interactions_s2 AS double) - 1 AS variation
 FROM s1 LEFT JOIN s2 on s1.street=s2.street
 ORDER BY  share_of_total_interactions_s1 DESC
 {limit}
