13ea7d9c42e754598a516eedbb5c3899118c9e8e
Hugo Barreto <hugobarreto94@gmail.com>
2019-11-25 19:23:46 -0300

    Sidebar Add - Error Flux
    Former-commit-id: cd8b14f09e9627cb33b1da1e8059d6128073b98e

--- dashboard/src/routes.js
+++ dashboard/src/routes.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 
+import DefaultLayout from './layouts';
 import WeeklyReport from './pages/WeeklyReport';
 
 export default function Routes() {
   return (
     <Switch>
       <Redirect exact from="/" to="/weekly" />
-      <Route path="/weekly" component={WeeklyReport} />
+      <Route
+        path="/weekly"
+        component={
+          <DefaultLayout noNavbar noFooter>
+            <WeeklyReport />
+          </DefaultLayout>
+        }
+      />
     </Switch>
   );
 }
