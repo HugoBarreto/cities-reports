50a730354631743a05f919e1783c03ab9269d0f3
Hugo Barreto <hugobarreto94@gmail.com>
2019-11-29 20:44:25 -0300

    Bug do Kepler resolvido retirando Reactotron
    Former-commit-id: 49f3114af09221f7aaabaab76610405b277ce58b

--- dashboard/src/store/index.js
+++ dashboard/src/store/index.js
@@ -1,28 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { taskMiddleware } from 'react-palm/tasks';
-import createSagaMiddleware from 'redux-saga';
+import { enhanceReduxMiddleware } from 'kepler.gl/middleware';
 
 import rootReducer from './modules/rootReducer';
-import rootSaga from './modules/rootSaga';
 
-const sagaMonitor =
-  process.env.NODE_ENV === 'development'
-    ? console.tron.createSagaMonitor()
-    : null;
-
-const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
-
-const middlewares = [taskMiddleware, sagaMiddleware];
-const enhancers =
+const middlewares = enhanceReduxMiddleware([]);
+/* const enhancers =
   process.env.NODE_ENV === 'development'
     ? [console.tron.createEnhancer(), applyMiddleware(...middlewares)]
     : [applyMiddleware(...middlewares)];
+ */
+const enhancers = [applyMiddleware(...middlewares)];
 
 const initialState = {};
 
 const store = createStore(rootReducer, initialState, compose(...enhancers));
-
-sagaMiddleware.run(rootSaga);
 
 export { default as Constants } from './constants';
 export default store;
