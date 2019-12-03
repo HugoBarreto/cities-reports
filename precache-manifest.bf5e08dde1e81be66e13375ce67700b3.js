ad11d15370927b03e03e219a95293dc15185bd35
Hugo Barreto <hugobarreto94@gmail.com>
2019-12-03 13:33:40 -0300

    Updates

---
+++ precache-manifest.bf5e08dde1e81be66e13375ce67700b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d2ebc3ba9fdb1a8fff628b7554d2e2",
+    "url": "/cities-reports/index.html"
+  },
+  {
+    "revision": "e98d6f083dff4c78718a",
+    "url": "/cities-reports/static/css/2.a27340cf.chunk.css"
+  },
+  {
+    "revision": "fa3817157036698daa8d",
+    "url": "/cities-reports/static/css/main.780a9e14.chunk.css"
+  },
+  {
+    "revision": "e98d6f083dff4c78718a",
+    "url": "/cities-reports/static/js/2.c1e42b9f.chunk.js"
+  },
+  {
+    "revision": "fa3817157036698daa8d",
+    "url": "/cities-reports/static/js/main.1c86cd63.chunk.js"
+  },
+  {
+    "revision": "a98e0260a3c6093c9674",
+    "url": "/cities-reports/static/js/runtime-main.9f212820.js"
+  },
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "/cities-reports/static/media/shards-dashboards-logo.60a85991.svg"
+  }
+]);
